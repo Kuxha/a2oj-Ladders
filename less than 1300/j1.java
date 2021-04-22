ea04ee0f54509b443114e87fa589e6c36109c2a2
Laba Kumar Deka <alukardmakabe@gmail.com>
2021-04-22 22:07:45 +0530

    Add files via upload

---
+++ less than 1300/j1.java
@@ -0,0 +1,73 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class j1 {
+
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task solver = new Task();
+        solver.solve(1, in, out);
+        out.close();
+    }
+
+    static class Task {
+
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+
+            int t = in.nextInt();
+            int x = 0, y = 0, z = 0;
+
+            for (int i = 0; i < t; i++) {
+
+                x += in.nextInt();
+                y += in.nextInt();
+                z += in.nextInt();
+            }
+
+            if(x == 0 && y== 0 && z ==0 ) out.println("YES");
+            else out.println("NO");
+
+        }
+    }
+
+    static class InputReader {
+
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+    }
+}
+
+
